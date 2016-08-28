730228bcdae0e7e5a1779671d056904ac23ef2ab
mdhell <mdhellstrom@gmail.com>
2016-08-28 18:42:29 +0000

    added test email

--- app.js
+++ app.js
@@ -3,17 +3,25 @@ var express = require("express");
 var app = express();
 // using SendGrid's Node.js Library
 // https://github.com/sendgrid/sendgrid-nodejs
-var sendgrid  = require('sendgrid')(process.env.SENDGRID_USERNAME, process.env.SENDGRID_PASSWORD);
-sendgrid.send({
-  to:       'mdhellstrom@gmail.com',
-  from:     'mdhell@aol.com',
-  subject:  'Hello World',
-  text:     'My first email through SendGrid.'
-}, function(err, json) {
-  if (err) { return console.error(err); }
-  console.log(json);
+var helper = require('sendgrid').mail;
+var from_email = new helper.Email('test@example.com');
+var to_email = new helper.Email('test@example.com');
+var subject = 'Hello World from the SendGrid Node.js Library!';
+var content = new helper.Content('text/plain', 'Hello, Email!');
+var mail = new helper.Mail(from_email, subject, to_email, content);
+
+var sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+var request = sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: mail.toJSON(),
 });
 
+sg.API(request, function(error, response) {
+  console.log(response.statusCode);
+  console.log(response.body);
+  console.log(response.headers);
+});
 
 
 
