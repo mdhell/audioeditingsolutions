101d2f8977411784858b39cf32880367be17a78a
mdhell <mdhellstrom@gmail.com>
2016-09-11 00:51:04 +0000

    added coverr and made landing page. CSS changes to navbar

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.static(__dirname + "/public"));
 
 
 app.get("/", function(req, res) {
-   res.render("home"); 
+   res.render("landing"); 
 });
 
 app.get("/", function(req, res) {
